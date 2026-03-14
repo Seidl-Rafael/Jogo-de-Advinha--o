7e41308b25246d2a9b94f279539f7cd133ba2d4a
Rafael Seidl <seidlrafael2006@gmail.com>
2026-03-14 19:29:24 -0300

    Projeto Feito v1

---
+++ jogo.js
@@ -0,0 +1,48 @@
+let x = 0;
+let vidas = 10;
+let game = false;
+
+function iniciar() {
+    game = true;
+    vidas = 10;
+    document.getElementById("life").textContent = vidas;
+    document.getElementById("chute").value = "";
+    x = Math.floor(Math.random() * 100 + 1);
+    document.getElementById("result").textContent = "Número gerado. Boa Sorte!"
+}
+
+function kick() {
+
+    if (game === false) {
+        return;
+    }
+
+    let number = parseInt(document.getElementById("chute").value);
+
+    if (vidas === 0) {
+        document.getElementById("result").textContent = "Suas vidas acabaram. O número era " + x;
+        game = false;
+    }
+
+    while (vidas != 0) {
+
+        if (number === x) {
+            document.getElementById("result").textContent = "Parabéns, você venceu!";
+            game = false;
+            break;
+        } else if (number < x) {
+            document.getElementById("result").textContent = "O número secreto é maior que " + number;
+            vidas = vidas - 1;
+            document.getElementById("life").textContent = vidas;
+            document.getElementById("chute").value = "";
+            break;
+        } else {
+            document.getElementById("result").textContent = "O número secreto é menor que " + number;
+            vidas = vidas - 1;
+            document.getElementById("life").textContent = vidas;
+            document.getElementById("chute").value = "";
+            break;
+        }
+
+    }
+}
